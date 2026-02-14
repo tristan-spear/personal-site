2961097b39a8ca158951f97a535e63739b0535f7
tristan-spear <tspear1704@gmail.com>
2026-02-13 17:20:28 -0800

    committing progress

--- client/src/routes/contact/contact.jsx
+++ client/src/routes/contact/contact.jsx
@@ -85,6 +85,61 @@ function Contact() {
               </div>
             </form>
           </div>
+          <div className="contact-cube-wrap">
+            <div className="contact-cube-container">
+              <div className="contact-cube">
+                <div style={{ '--x': -1, '--y': 0 }}>
+                  <span style={{ '--i': 3 }} />
+                  <span style={{ '--i': 2 }} />
+                  <span style={{ '--i': 1 }} />
+                </div>
+                <div style={{ '--x': 0, '--y': 0 }}>
+                  <span style={{ '--i': 3 }} />
+                  <span style={{ '--i': 2 }} />
+                  <span style={{ '--i': 1 }} />
+                </div>
+                <div style={{ '--x': 1, '--y': 0 }}>
+                  <span style={{ '--i': 3 }} />
+                  <span style={{ '--i': 2 }} />
+                  <span style={{ '--i': 1 }} />
+                </div>
+              </div>
+              <div className="contact-cube">
+                <div style={{ '--x': -1, '--y': 0 }}>
+                  <span style={{ '--i': 3 }} />
+                  <span style={{ '--i': 2 }} />
+                  <span style={{ '--i': 1 }} />
+                </div>
+                <div style={{ '--x': 0, '--y': 0 }}>
+                  <span style={{ '--i': 3 }} />
+                  <span style={{ '--i': 2 }} />
+                  <span style={{ '--i': 1 }} />
+                </div>
+                <div style={{ '--x': 1, '--y': 0 }}>
+                  <span style={{ '--i': 3 }} />
+                  <span style={{ '--i': 2 }} />
+                  <span style={{ '--i': 1 }} />
+                </div>
+              </div>
+              <div className="contact-cube">
+                <div style={{ '--x': -1, '--y': 0 }}>
+                  <span style={{ '--i': 3 }} />
+                  <span style={{ '--i': 2 }} />
+                  <span style={{ '--i': 1 }} />
+                </div>
+                <div style={{ '--x': 0, '--y': 0 }}>
+                  <span style={{ '--i': 3 }} />
+                  <span style={{ '--i': 2 }} />
+                  <span style={{ '--i': 1 }} />
+                </div>
+                <div style={{ '--x': 1, '--y': 0 }}>
+                  <span style={{ '--i': 3 }} />
+                  <span style={{ '--i': 2 }} />
+                  <span style={{ '--i': 1 }} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
